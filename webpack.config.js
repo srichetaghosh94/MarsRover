b9489ecb96ce02d45278fb2559d9d6cab5bb2c89
srichetaghosh94 <91406940+srichetaghosh94@users.noreply.github.com>
2021-09-26 11:34:36 +0200

    adding to root folder

---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+const path = require('path')
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'public'),
+    filename: 'bundle.js',
+  },
+  performance: {
+    maxEntrypointSize: 1024000,
+    maxAssetSize: 1024000
+  },
+  devServer: {
+    publicPath: '/public/',
+    compress: true,
+    port: 9000,
+    hot: true,
+  },
+}
